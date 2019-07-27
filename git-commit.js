35629665c526b3a4d88d1c607f68204b4d77a832
jbhouse <jbmoorhouse1@gmail.com>
2019-07-26 22:53:14 -0400

    git-commands: added

--- git-commit.js
+++ git-commit.js
@@ -1,19 +1,25 @@
 const git = require('simple-git');
 const commitMessage = process.argv[3];
 const currentWorkingDirectory = process.argv[2];
+const branch = require('git-branch');
 
-git(currentWorkingDirectory).raw(
-    [
-        'commit',
-        '.',
-        '-m',
-        commitMessage
-    ], (err, result) => {
+branch(currentWorkingDirectory)
+    .then(name => {
+        git(currentWorkingDirectory).raw(
+            [
+                'commit',
+                '.',
+                '-m',
+                name + ": " + commitMessage
+            ], (err, result) => {
 
-        if (Boolean(result)) {
-            console.log(result);
-        }
-        if (Boolean(err)) {
-            console.log(err);
-        }
-    });
+                if (Boolean(result)) {
+                    console.log(result);
+                }
+                if (Boolean(err)) {
+                    console.log(err);
+                }
+            });
+    }) //=> 'master'
+    .catch(console.error);
+
