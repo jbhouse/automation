f63ec5ff1ea7138469d2816a4a7ed8046fdb6367
jbhouse <jbmoorhouse1@gmail.com>
2019-07-26 23:09:41 -0400

    git-commands: updated git commit script to include branch name

--- git-commit.js
+++ git-commit.js
@@ -1,16 +1,22 @@
 const git = require('simple-git');
 const currentWorkingDirectory = process.argv[2];
-const commitMessage = process.argv[3];
+var commitMessage = "";
 const branch = require('git-branch');
+
+for (let i = 3; i < process.argv.length; i++) {
+    commitMessage += (" " + process.argv[i])
+}
 
 branch(currentWorkingDirectory)
     .then(name => {
+        console.log(name + ": " + commitMessage);
+
         git(currentWorkingDirectory).raw(
             [
                 'commit',
                 '.',
                 '-m',
-                name + ": " + commitMessage
+                name + ":" + commitMessage
             ], (err, result) => {
 
                 if (Boolean(result)) {
