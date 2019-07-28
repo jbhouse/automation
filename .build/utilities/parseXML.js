a7dbc0a295bb1ae878229e1c194332fd0bf2b565
jbhouse <jbmoorhouse1@gmail.com>
2019-07-28 18:09:02 -0400

    xml-parsing: moved more files over to being typescript based. scaffolded out the start of xml parsing

--- .build/utilities/parseXML.js
+++ .build/utilities/parseXML.js
@@ -1,6 +1,20 @@
 "use strict";
-var cmd = require('./commandLine');
-var filters = require('./filters.js');
-filters.filterForFileName(cmd.changeDirectoryTo(process.argv[2]), "pom.xml").forEach(function (file) {
-    console.log(file);
+// var cmd = require('./commandLine');
+// var filters = require('./filters.js')
+// filters.filterForFileName(cmd.changeDirectoryTo(process.argv[2]), "pom.xml").forEach((file: string) => {
+//     console.log(file);
+// });
+var xml2js = require('xml2js');
+var fs = require('fs');
+var parser = new xml2js.Parser({ attrkey: "ATTR" });
+// this example reads the file synchronously
+// you can read it asynchronously also
+var xml_string = fs.readFileSync(process.argv[2], "utf8");
+parser.parseString(xml_string, function (error, result) {
+    if (error === null) {
+        console.log(result);
+    }
+    else {
+        console.log(error);
+    }
 });
--- utilities/parseXML.ts
+++ utilities/parseXML.ts
@@ -1,6 +1,16 @@
-var cmd = require('./commandLine');
-var filters = require('./filters.js')
+var xml2js = require('xml2js');
+var fs = require('fs');
+var parser = new xml2js.Parser({ attrkey: "ATTR" });
 
-filters.filterForFileName(cmd.changeDirectoryTo(process.argv[2]), "pom.xml").forEach((file: string) => {
-    console.log(file);
+// this example reads the file synchronously
+// you can read it asynchronously also
+let xml_string = fs.readFileSync(process.argv[2], "utf8");
+
+parser.parseString(xml_string, function (error: string, result: string) {
+    if (error === null) {
+        console.log(result);
+    }
+    else {
+        console.log(error);
+    }
 });
