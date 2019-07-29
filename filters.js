1983612077be6da9673c8afa509da333c95c8d47
jbhouse <jbmoorhouse1@gmail.com>
2019-07-29 12:32:03 -0400

    develop: added placeholder typescript file, added node and express type dependencies to allow users to re-compile without needing to install anything globally

--- filters.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const cmd = require('./commandLine');
-module.exports = {
-    isGitProject: (absolutePath) => fs.lstatSync(absolutePath).isDirectory() && cmd.checkIfFileExists(absolutePath + '\\.git'),
-    filterForGitProjects: (projects) => projects.filter((project) => fs.lstatSync(project).isDirectory() && cmd.checkIfFileExists(project + '\\.git'))
-}
