2f4855da86a6e08a342fb03de1522c215b9cc893
jbhouse <jbmoorhouse1@gmail.com>
2019-08-20 23:39:09 -0400

    auto-pr: replaced for loop with slice

--- .build/bin/main.js
+++ .build/bin/main.js
@@ -1,14 +1,10 @@
 "use strict";
 var gitCommands = require('../utilities/git.js');
 var commandToInvoke = process.argv[2];
-var additionalArguments = [];
-for (var i = 4; i < process.argv.length; i++) {
-    additionalArguments.push(process.argv[i]);
-}
 var commandMap = {
     "gitUpdate": function () { return gitCommands.update(process.argv[3]); },
     "gitPull": function () { return gitCommands.pull(process.argv[3], process.argv[4]); },
-    "gitCommit": function () { return gitCommands.commit(process.argv[3], additionalArguments); },
+    "gitCommit": function () { return gitCommands.commit(process.argv[3], process.argv.slice(4)); },
     "goToGithub": function () { return gitCommands.goToGithub(process.argv[3]); }
 };
 commandMap[commandToInvoke]();
