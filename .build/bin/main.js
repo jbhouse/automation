66a970dff16f2acdb6fbc7daafc3ee1d53434035
jbhouse <jbmoorhouse1@gmail.com>
2019-08-25 17:37:06 -0400

    develop: split out branch name parsing logic from comitting logic

--- .build/bin/main.js
+++ .build/bin/main.js
@@ -4,10 +4,10 @@ var commandToInvoke = process.argv[2];
 var commandMap = {
     "gitUpdate": function () { return gitCommands.update(process.argv[3]); },
     "gitPull": function () { return gitCommands.pull(process.argv[3], process.argv[4]); },
-    "gitCommit": function () { return gitCommands.commit(process.argv[3], process.argv.slice(4)); },
-    "goToGithub": function () { return gitCommands.goToGithub(process.argv[3]); }
-    // ,"openPR": () => gitCommands.openPR(process.argv[3])
-    ,
-    "openPR": function () { return gitCommands.openPR(process.argv[3]); }
+    "gitCommit": function () { return gitCommands.commit(process.argv[3], process.argv.slice(4).join(" ")); },
+    "goToGithub": function () { return gitCommands.goToGithub(process.argv[3]); },
+    "openPR": function () { return gitCommands.openPR(process.argv[3]); },
+    "parseBranch": function () { return gitCommands.parseBranch(process.argv[3]); },
+    "commitBranchName": function () { return gitCommands.parseBranch(process.argv[3]).then(function (branchName) { return gitCommands.commit(process.argv[3], branchName + ": " + process.argv.slice(4).join(" ")); }); }
 };
 commandMap[commandToInvoke]();
--- utilities/git.ts
+++ utilities/git.ts
@@ -1,13 +1,15 @@
-let gitUpdate = require('../utilities/gitUpdate.js');
-let gitPull = require('../utilities/gitPull.js');
-let gitCommit = require('../utilities/gitCommit.js');
-let goToGithub = require('../utilities/goToGithub.js');
-let openPr = require('../utilities/openPR.js');
+let gitUpdate = require('../utilities/gitUpdate');
+let gitPull = require('../utilities/gitPull');
+let gitCommit = require('../utilities/gitCommit');
+let goToGithub = require('../utilities/goToGithub');
+let openPr = require('../utilities/openPR');
+let parseBranchName = require('../utilities/parseBranchName');
 
 module.exports = {
-    update: (path: string) => gitUpdate.gitUpdate(path),
-    pull: (workingDirectory: string, branchName: string) => gitPull.gitPull(workingDirectory, branchName),
-    commit: (workingDirectory: string, commitMessage: string[]) => gitCommit.gitCommit(workingDirectory, commitMessage),
-    goToGithub: (executablePath: string) => goToGithub.goToGithub(executablePath),
-    openPR: (executablePath: string) => openPr.openPR(executablePath)
+    update: (path: string) => gitUpdate.gitUpdate(path)
+    , pull: (workingDirectory: string, branchName: string) => gitPull.gitPull(workingDirectory, branchName)
+    , commit: (workingDirectory: string, commitMessage: string[]) => gitCommit.gitCommit(workingDirectory, commitMessage)
+    , goToGithub: (executablePath: string) => goToGithub.goToGithub(executablePath)
+    , openPR: (executablePath: string) => openPr.openPR(executablePath)
+    , parseBranch: (executablePath: string) => parseBranchName.parseGitBranch(executablePath)
 }
