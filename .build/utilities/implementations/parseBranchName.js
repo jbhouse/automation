edf6456398cd75d6523691209ecc1eca9a5f199b
jbhouse <jbmoorhouse1@gmail.com>
2019-11-29 19:00:48 -0500

    git-utils: replaced git-branch dependency with child process in parseBranchName file

--- .build/utilities/implementations/parseBranchName.js
+++ .build/utilities/implementations/parseBranchName.js
@@ -1,7 +1,7 @@
 "use strict";
+var { execSync } = require("child_process");
 module.exports = {
-    // find a way to replace this with childprocess, or maybe simple git
-    parseGitBranch: (pathName) => require('git-branch')(pathName)
-        .then((name) => name)
-        .catch(console.error)
+    parseGitBranch: function parseGitBranch(cwd) {
+        return execSync("git branch -a", { cwd, encoding: "utf8" }).split("\n").filter((branchName) => branchName.includes("*"))[0].replace("* ", "");
+    }
 };
--- utilities/implementations/parseBranchName.ts
+++ utilities/implementations/parseBranchName.ts
@@ -1,7 +1,6 @@
+
 module.exports = {
-    // find a way to replace this with childprocess, or maybe simple git
-    parseGitBranch: (pathName: string) =>
-        require('git-branch')(pathName)
-            .then((name: string) => name)
-            .catch(console.error)
+    parseGitBranch: function parseGitBranch(cwd: any) {
+        return require("child_process")("git branch -a", { cwd, encoding: "utf8" }).split("\n").filter((branchName: string) => branchName.includes("*"))[0].replace("* ", "");
+    }
 }
