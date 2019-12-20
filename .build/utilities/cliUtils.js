baf69d6c9fb2ce468c554cab2d9a75e37be82955
jbhouse <jbmoorhouse1@gmail.com>
2019-12-19 21:02:02 -0500

    sqlCommads: added a function to search mdn for the javascript syntax you need

--- .build/utilities/cliUtils.js
+++ .build/utilities/cliUtils.js
@@ -3,8 +3,10 @@ module.exports = (() => {
     const google = require('./implementations/google');
     const maven = require('./implementations/FilterCommandOutput');
     const childProcess = require('child_process');
+    const javascriptSyntax = require('../utilities/implementations/javascriptSyntax');
     return {
         google: (executablePath, query) => google.searchGoogle(executablePath, "https://www.google.com/search?q=" + query.slice(4).join("+")),
-        FilterOutput: (command, filterKeys) => maven(childProcess).filterOutput(command, filterKeys)
+        FilterOutput: (command, filterKeys) => maven(childProcess).filterOutput(command, filterKeys),
+        searchForJavascriptSyntax: (browserExecutablePath, userInput) => javascriptSyntax.getJavascriptUrl(google.searchGoogle, browserExecutablePath, userInput)
     };
 })();
